1d3988a4ccb4bd0fc2ac0244a4dce171cc803aac
DongNguyen <dongnguyen.na1@gmail.com>
2020-04-13 20:09:15 +0700

    delete all

--- resources/js/fontawesome.js
+++
@@ -1,8 +0,0 @@
-import fontawesome from '@fortawesome/fontawesome';
-import { faCaretUp } from '@fortawesome/free-solid-svg-icons/faCaretUp';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
-import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
-import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
-import { faComment } from '@fortawesome/free-solid-svg-icons/faComment';
-import { faEye } from '@fortawesome/free-solid-svg-icons/faEye';
-fontawesome.library.add([faCaretUp, faCaretDown, faStar, faCheck, faComment, faEye]);
